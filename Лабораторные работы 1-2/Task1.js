577f927e08bbec113f590811b2fdcd2a5891f426
EDEN-HALE <edenitgod@gmail.com>
2025-11-05 00:02:24 +0300

    05.11.2025

---
+++ Лабораторные работы 1-2/Task1.js
@@ -0,0 +1,10 @@
+function revrseList(arr){
+    const newArr = [];
+    for (let i = arr.length -1 ; i >=  0; i--){
+        newArr.push(arr[i])
+    }
+    return newArr;
+}
+
+
+console.log(revrseList([1, 2, 3, 4, 5]))
